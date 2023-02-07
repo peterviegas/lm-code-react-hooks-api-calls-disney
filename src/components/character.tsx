875445bc411c6a0f764d502d66eab23c63a5269f
Peter Viegas <peterviegas@outlook.com>
2023-02-07 17:18:04 +0000

    Modified Component character.tsx - Include image default and change struct include retrurn

--- src/components/character.tsx
+++ src/components/character.tsx
@@ -2,8 +2,16 @@ import { DisneyCharacter } from "../disney_character"
 
 // for our props we can reuse the DisneyCharacter interface
 // - defining an anonymous type that just has one property - a DisneyCharacter
-const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => 
-  
+const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) => {
+
+  // Define a default in case the character doesn't have an image
+  let imageSrc = "https://picsum.photos/300/200/?blur";
+  if (character.imageUrl) {
+    // API seems to include extra path for images so here we strip it off to fetch raw image
+    imageSrc = character.imageUrl.substring(0, character.imageUrl.indexOf('/revision'));
+  }
+
+  return(
     <article className="character-item">
 
       <h2>{character.name}</h2>
@@ -15,6 +23,9 @@ const Character : React.FC<{ character: DisneyCharacter}> = ( { character }) =>
       <img className="character-item__img" src={character.imageUrl} alt={character.name} />
 
     </article>
+  )
+}
+  
   
 
 
